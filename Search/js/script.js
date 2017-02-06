d012870a7f615fb7ea82ad03fbef59762c3b587a
ruslanprikhdk <ruslanprikhdk@gmail.com>
2017-02-06 12:14:30 +0200

    Add files via upload

--- Search/js/script.js
+++ Search/js/script.js
@@ -1,74 +1,55 @@
-﻿
-function Render(response)
-{
-      var title = $("#title").val();
-      var ul = $("#items");
-      ul.empty();
-      var arr = response.tags;
-      for (i = 0; i < arr.length; i++)
-        {
-        var data = arr[i];
-        var result = data.searchterm;
-        var image = data.image;
-            var p = $("<p>").append(result);
-            var li = $("<li>").append(p);
-            $("<img>")
-              .attr("src", image)
-              .appendTo(li);
-            ul.append(li);
-        }
+﻿$(function () {
+    $("#getItem").click(function Start() {
+      var $title = $("#title").val();
+      var $url = "https://api.tenor.co/v1/search?key=LIVDSRZULELA&tag=" + $title + "&locale=en_US&safesearch=moderate";
+      console.log($url);
+      $.ajax({
+        type: "GET",
+        url: $url,
+        dataType: 'json',
+        success: Render
+      });
+    });
 
-      $("#getItem").click(function Start() {
+    $("#title").on("keypress", function(event) {
+      if(event.which==13) {
+        event.preventDefault();
+        var $title = $("#title").val();
+        var $url = "https://api.tenor.co/v1/search?key=LIVDSRZULELA&tag=" + $title + "&locale=en_US&safesearch=moderate";
+        console.log($url);
+        $.ajax({
+          type: "GET",
+          url: $url,
+          dataType: 'json',
+          success: Render
+        });
+      }
+    });
+    
+});
+
+
+
+function Render(response)
+{     
+       
         var ul = $("#items");
         ul.empty();
-        var title = $("#title").val();
+        var arr = response.results;
+        var $title = $("#title").val();
+        var $url = "https://api.tenor.co/v1/search?key=LIVDSRZULELA&tag=" + $title + "&locale=en_US&safesearch=moderate";
         for (i = 0; i < arr.length; i++)
           {
           var data = arr[i];
-          var result = data.searchterm;
-          var image = data.image
-          if(result == title)
-            {
-              var p = $("<p>").append(result);
-              var li = $("<li>").append(p);
-              $("<img>")
-                .attr("src", image)
-                .appendTo(li);
-                ul.append(li);
-            }
+          var result = data.title;
+          var image = data.media[0].gif.preview;
+          var p = $("<p>").append(result);
+          var li = $("<li>").append(p);
+          $("<img>")
+            .attr("src", image)
+            .appendTo(li);
+              ul.append(li);
           }
-        });
-
-      $("#title").on("keypress", function(event) {
-           if(event.which==13) {
-             event.preventDefault();
-             }
-             ul.empty();
-             var title = $("#title").val();
-             for (i = 0; i < arr.length; i++)
-               {
-               var data = arr[i];
-               var result = data.searchterm;
-               var image = data.image
-               if(result == title)
-                 {
-                   var p = $("<p>").append(result);
-                   var li = $("<li>").append(p);
-                   $("<img>")
-                     .attr("src", image)
-                     .appendTo(li);
-                     ul.append(li);
-                 }
-               }
-         });
 
 }
 
-$(function () {
-    $.ajax({
-      url: "https://api.tenor.co/v1/tags?key=LIVDSRZULELA&searchterm="+title+"&type=featured,explore",
-      dataType: 'json',
-      success: Render,
-    });
-
-});
